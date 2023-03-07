b892809852e81d8c3b20d787ed52ef24d9e9dc30
joastek <joastek@gmail.com>
2023-03-07 21:35:52 +0100

    Some problems with css

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 //drums
-let soundA = new Audio("sounds/boom.wav");
+let soundA = new Audio("sounds/clap.wav");
+let soundC = new Audio("sounds/hihat.wav");
+const btnHideRectangle = document.querySelector(".container-box");
 document.addEventListener("keydown", function (event) {
   if (event.keyCode == 65) {
+    soundA.currentTime = 0;
     soundA.play();
+    btnHideRectangle.style.borderLeft = "25px solid green";
+  } else if (event.keyCode == 83) {
+    soundC.currentTime = 0;
+    soundC.play();
   }
 });
-DOMMatrixReadOnly;
